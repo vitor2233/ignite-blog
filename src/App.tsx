35b9aaf48487566eb7f2451f470de0be592d1925
vitor2233 <vitorguilherme2233@gmail.com>
2024-02-29 21:39:53 -0300

    Changing to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Header } from "./components/Header"
 import "./global.css"
 import styles from "./App.module.css"
 import { Sidebar } from "./components/Sidebar"
-import { Post } from "./components/Post"
+import { Post, PostType } from "./components/Post"
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -48,9 +48,7 @@ export function App() {
             return (
               <Post
                 key={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
+                post={post}
               />
             )
           })}
